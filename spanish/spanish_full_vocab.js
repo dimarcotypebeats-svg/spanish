ab9d729ff99c9c8d92f7bb63b1cee24fd7565182
dimarcotypebeats-svg <dimarcotypebeats@gmail.com>
2025-08-31 13:11:38 -0400

    Add files via upload

---
+++ spanish/spanish_full_vocab.js
@@ -0,0 +1,117 @@
+// spanish_full_vocab.js
+window.SPANISH_DATA = {
+  "Basics": [
+    {
+      "english": "and",
+      "spanish": "Y",
+      "phonetic": "ee"
+    },
+    {
+      "english": "or",
+      "spanish": "O",
+      "phonetic": "oh"
+    },
+    {
+      "english": "but",
+      "spanish": "Pero",
+      "phonetic": "PEH-roh"
+    },
+    {
+      "english": "because",
+      "spanish": "Porque",
+      "phonetic": "POR-keh"
+    },
+    {
+      "english": "also, too",
+      "spanish": "También",
+      "phonetic": "tahm-BYEN"
+    }
+  ],
+  "People": [
+    {
+      "english": "Dudette",
+      "spanish": "Tía",
+      "phonetic": "TEE-ah"
+    },
+    {
+      "english": "Dude, Guy (Spain)",
+      "spanish": "Tío",
+      "phonetic": "TEE-oh"
+    },
+    {
+      "english": "Friend (masculine)",
+      "spanish": "Amigo",
+      "phonetic": "ah-MEE-goh"
+    }
+  ],
+  "Places": [
+    {
+      "english": "the bathroom",
+      "spanish": "el baño",
+      "phonetic": "el BAH-nyoh"
+    },
+    {
+      "english": "the restaurant",
+      "spanish": "el restaurante",
+      "phonetic": "el rehs-tow-RAHN-teh"
+    },
+    {
+      "english": "the hotel",
+      "spanish": "el hotel",
+      "phonetic": "el oh-TEL"
+    }
+  ],
+  "Things": [
+    {
+      "english": "Monday",
+      "spanish": "el lunes",
+      "phonetic": "el LOO-ness"
+    },
+    {
+      "english": "Tuesday",
+      "spanish": "el martes",
+      "phonetic": "el MAR-tess"
+    },
+    {
+      "english": "Wednesday",
+      "spanish": "el miércoles",
+      "phonetic": "el MYER-koh-less"
+    }
+  ],
+  "Adjectives": [
+    {
+      "english": "kind",
+      "spanish": "amable",
+      "phonetic": "ah-MAH-bleh"
+    },
+    {
+      "english": "nice, mean",
+      "spanish": "simpático, simpática, antipático, antipática",
+      "phonetic": "seem-PAH-tee-koh, seem-PAH-tee-kah, an-tee-PAH-tee-koh, an-tee-PAH-tee-kah"
+    },
+    {
+      "english": "funny",
+      "spanish": "gracioso, graciosa",
+      "phonetic": "grah-SYOH-soh, grah-SYOH-sah"
+    }
+  ],
+  "Verbs": [
+    {
+      "english": "I move",
+      "spanish": "yo muevo",
+      "phonetic": "MWEH-boh"
+    },
+    {
+      "english": "you move",
+      "spanish": "tú mueves",
+      "phonetic": "MWEH-behs"
+    },
+    {
+      "english": "he moves, she moves, you move (formal)",
+      "spanish": "él mueve, ella mueve, usted mueve",
+      "phonetic": "MWEH-beh"
+    }
+  ]
+} ;
+
+
